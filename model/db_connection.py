4fecbc94ed9bad8a82af03e0d6862f313c79a0df
Bagus Ilman Huda <117415506+Bagusilmn@users.noreply.github.com>
2025-05-23 09:57:25 +0700

    Update db_connection.py

--- model/db_connection.py
+++ model/db_connection.py
@@ -1,14 +1,51 @@
+import os
 import streamlit as st
-from streamlit_gsheets import GSheetsConnection 
+
+# Runtime patch: jika `st.secrets` kosong (deployment), isi dari env
+if not st.secrets:
+    st.secrets._secrets = {
+        "connections": {
+            "internship_RLO": {
+                "dialect": os.getenv("INTERNSHIP_RLO_DIALECT"),
+                "driver": os.getenv("INTERNSHIP_RLO_DRIVER"),
+                "host": os.getenv("INTERNSHIP_RLO_HOST"),
+                "port": int(os.getenv("INTERNSHIP_RLO_PORT")),
+                "database": os.getenv("INTERNSHIP_RLO_DATABASE"),
+                "username": os.getenv("INTERNSHIP_RLO_USERNAME"),
+                "password": os.getenv("INTERNSHIP_RLO_PASSWORD"),
+            },
+            "ADOMobile": {
+                "spreadsheet": os.getenv("ADOMOBILE_SPREADSHEET")
+            },
+            "MarketCompetition": {
+                "spreadsheet": os.getenv("MARKETCOMPETITION_SPREADSHEET")
+            },
+            "ADOIH": {
+                "spreadsheet": os.getenv("ADOIH_SPREADSHEET")
+            }
+        },
+        "auth": {
+            "redirect_uri": os.getenv("REDIRECT_URI"),
+            "cookie_secret": os.getenv("COOKIE_SECRET"),
+        },
+        "auth.google": {
+            "client_id": os.getenv("GOOGLE_CLIENT_ID"),
+            "client_secret": os.getenv("GOOGLE_CLIENT_SECRET"),
+            "server_metadata_url": os.getenv("GOOGLE_METADATA_URL"),
+        }
+    }
+
+# Streamlit connection functions
+from streamlit_gsheets import GSheetsConnection
 
 def get_connection():
     return st.connection('internship_RLO', type='sql')
 
-def gsheet_ADOMobile_connection() :
+def gsheet_ADOMobile_connection():
     return st.connection("ADOMobile", type=GSheetsConnection)
 
-def gsheet_ADOIH_connection() :
+def gsheet_ADOIH_connection():
     return st.connection("ADOIH", type=GSheetsConnection)
 
-def gsheet_MarketCompetition_connection() :
+def gsheet_MarketCompetition_connection():
     return st.connection("MarketCompetition", type=GSheetsConnection)
